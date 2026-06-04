dc298f0514ade27168168199488998cc21d9cd12
Munavwar Shaik <munavwar0125@gmail.com>
2026-06-04 11:18:57 +0530

    update script.js

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 let display = document.querySelector(".display");
-// Add values to display
+
 function appendValue(value){
   display.value += value;
 }
-// Clear everything
+
 function clearDisplay(){
   display.value = "";
 }
-// Delete last character
+
 function deleteLast(){
   display.value = display.value.slice(0,-1);
 }
-// Calculate answer
+
 function calculate(){
   try{
     display.value = eval(display.value);
