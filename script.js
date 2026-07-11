2f726755cc658dccf199d3088ae98172a96caa05
Munavwar Shaik <munavwar0125@gmail.com>
2026-07-11 22:09:30 +0530

    update script.js

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
-let display = document.querySelector(".display");
+const display = document.getElementById("display");
 
-function appendValue(value){
+function appendValue(value) {
   display.value += value;
 }
 
-function clearDisplay(){
+function clearDisplay() {
   display.value = "";
 }
 
-function deleteLast(){
-  display.value = display.value.slice(0,-1);
+function deleteLast() {
+  display.value = display.value.slice(0, -1);
 }
 
-function calculate(){
-  try{
-    display.value = eval(display.value);
-  }
-  catch{
+function calculate() {
+  try {
+    // Only allow digits, operators, decimal points and spaces
+    if (!/^[0-9+\-*/.\s]*$/.test(display.value)) {
+      throw new Error("Invalid input");
+    }
+    display.value = Function('"use strict"; return (' + display.value + ")")();
+  } catch {
     display.value = "Error";
   }
 }
